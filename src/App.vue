2c67663bb620f0d1763679b9b2bf45e120c111f2
Thyago Teles Ferreira <thyago.ferreira@al.infnet.edu.br>
2020-02-19 18:34:52 -0300

    Implementação carrinho

--- src/App.vue
+++ src/App.vue
@@ -1,19 +1,20 @@
 <template>
   <div id="app">
     <b-container>
-      <h1><b-badge>{{ title }}</b-badge></h1>    
+      <h1>{{ title }}</h1>
 
-      <h3 v-if="horas >= 9 && horas < 17" id="aberto">Aberto</h3>
-      <h3 v-else-if="horas >= 17 && horas < 18" id="proximo-fechar">Próximo de Fechar</h3>
-      <h3 v-else id="fechado">Fechado</h3>
+      <h3 v-if="horas >= 9 && horas < 17" id="aberto" class="horario">Aberto</h3>
+      <h3 v-else-if="horas >= 17 && horas < 18" id="proximo-fechar" class="horario">Próximo de Fechar</h3>
+      <h3 v-else id="fechado" class="horario">Fechado</h3>
 
       <b-row>
         <b-col>
           <h2>Filmes encontrados</h2>
+          <b-button class="btn btn-primary btn-lg" @click="mostrarCarrinho">Carrinho: {{ quantidadeNoCarrinho }} filmes</b-button>
         </b-col>
       </b-row>      
 
-      <b-row class="cardss">
+      <b-row class="cardss" v-if="mostrarFilmes">
         <b-card v-bind:key="filme.id" v-for="filme in filmes" id="bcard"
           :title=filme.titulo
           img-src="https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcQJjFZSqrM6lP75FusvvBcU1wTPxXgqpBJgXE1Dre1YrrLJvMjK"
@@ -22,11 +23,16 @@
           tag="article"
           class="mb-2"
         >
+
           <b-card-text>{{ filme.descricao }}</b-card-text>
           <b-card-text>{{ filme.valor | formataPreco("R$") }}</b-card-text>
-          <b-button href="#" variant="primary">ALUGAR</b-button>
+          <b-button href="#" @click="adicionarAoCarrinho(filme)" v-if="validarPermissaoParaAdicionarNoCarrinho(filme)" variant="primary">ALUGAR</b-button>
+          <b-button disabled href="#" @click="adicionarAoCarrinho(filme)" v-else variant="primary">ALUGAR</b-button>
           
         </b-card>  
+      </b-row>
+      <b-row v-else>
+        <h2>Carrinho</h2>
       </b-row>
 
     </b-container>
@@ -37,17 +43,39 @@
 export default {
   name: 'App',
   components: {
-  }, data() {
+  },
+  data() {
     return {
-    title: 'Locadora de Filmes',
-    horas: new Date().getHours(),
-    filmes: [
-      { id: 1, titulo: "Shrek", descricao: "Um filme de ogro", valor: 15, imagem: "assets/logo.png" },
-      { id: 2, titulo: "007", descricao: "Um filme de espião", valor: 10, imagem: "assets/logo.png" },
-      { id: 3, titulo: "Frozen", descricao: "Um filme de gelo", valor: 20, imagem: "assets/logo.png" },
-      { id: 4, titulo: "O Albergue", descricao: "Um filme açougue", valor: 5, imagem: "assets/logo.png" },
-      { id: 5, titulo: "Click", descricao: "Um filme de comédia", valor: 25, imagem: "assets/logo.png" }
-    ]
+      mostrarFilmes: true,
+      title: 'Locadora de Filmes',
+      horas: new Date().getHours(),
+      filmes: [
+        { id: 1, titulo: "Shrek", descricao: "Um filme de ogro", valor: 15, imagem: "assets/logo.png", estoqueDisponivel: 3 },
+        { id: 2, titulo: "007", descricao: "Um filme de espião", valor: 10, imagem: "assets/logo.png", estoqueDisponivel: 5 },
+        { id: 3, titulo: "Frozen", descricao: "Um filme de gelo", valor: 20, imagem: "assets/logo.png", estoqueDisponivel: 7 },
+        { id: 4, titulo: "O Albergue", descricao: "Um filme açougue", valor: 5, imagem: "assets/logo.png", estoqueDisponivel: 4 },
+        { id: 5, titulo: "Click", descricao: "Um filme de comédia", valor: 25, imagem: "assets/logo.png", estoqueDisponivel: 6 }
+      ],
+      carrinho: []
+    } 
+  },
+  methods: {
+    mostrarCarrinho (){
+      this.mostrarFilmes = this.mostrarFilmes ? false : true;
+    },
+    adicionarAoCarrinho: function(filme) {
+      this.carrinho.push(filme.id);
+    },
+    quantidadeNoCarrinhoPorFilme: function(filme) {
+      return this.carrinho.filter(elem => elem === filme.id).length;
+    },
+    validarPermissaoParaAdicionarNoCarrinho: function(filme) {
+      return filme.estoqueDisponivel > this.quantidadeNoCarrinhoPorFilme(filme);
+    }
+  },
+  computed: {
+    quantidadeNoCarrinho: function () {
+      return this.carrinho.length;
     }
   }
 }
@@ -74,10 +102,14 @@ export default {
 #fechado {
   color: red;
 }
+.horario {
+  background-color: black;
+}
 .cardss {
   justify-content: space-between;
 }
 #bcard{
   max-width: 20rem;
 }
+
 </style>
